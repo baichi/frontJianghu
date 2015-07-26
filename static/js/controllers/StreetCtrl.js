fbd85d2c733488599f6f10f8202c4e1e29c84937
zhangrenyang-t510 <zhang_renyang@>
2015-07-26 14:56:37 +0800

    fix bugs

--- static/js/controllers/StreetCtrl.js
+++ static/js/controllers/StreetCtrl.js
@@ -1,7 +1,10 @@
 angular.module('frontModule').controller('StreetCtrl',function($scope,socket){
     $scope.messages = [];
     socket.on('allMessages',function(messages){
-        $scope.messages = messages;
+        $scope.messages.length = 0;
+        messages.forEach(function(message){
+            $scope.messages.push(message);
+        });
     });
     socket.emit('getAllMessages');
     socket.on('message',function(message){
